3f51bda0bf9ff0ef8208f9f7d7f573f7ba42859e
mrasheduzzaman <rashed091nsu@gmail.com>
2023-11-21 18:29:22 +0600

    Fix rust build

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,11 +1,11 @@
-use serde::{Deserialize, Serialize};
 use diesel::prelude::*;
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 use crate::schema::users;
 
 #[derive(Serialize, Deserialize, Queryable, Insertable)]
-#[table_name = "users"]
+#[diesel(table_name = users)]
 pub struct User {
     pub id: Uuid,
     pub name: String,
--- src/services/user_service.rs
+++ src/services/user_service.rs
@@ -1,13 +1,12 @@
 use diesel;
 use diesel::prelude::*;
-use uuid::Uuid;
+// use uuid::Uuid;
 
 use crate::common::database::Connection;
 use crate::models::user::User;
 
 use crate::schema::users;
-use crate::schema::users::dsl::users as all_users;
-
+// use crate::schema::users::dsl::users as all_users;
 
 pub struct UserService;
 
@@ -16,26 +15,26 @@ impl UserService {
         users::table.load::<User>(connection).unwrap()
     }
 
-    pub async fn create(connection: &mut Connection, new_user: &User) -> User {
-        let data = User {
-            id: Uuid::new_v4(),
-            name: new_user.name.clone(),
-            age: new_user.age.clone()
-        };
+    // pub async fn create(connection: &mut Connection, new_user: &User) -> User {
+    //     let data = User {
+    //         id: Uuid::new_v4(),
+    //         name: new_user.name.clone(),
+    //         age: new_user.age.clone()
+    //     };
 
-        let _result = diesel::insert_into(users::table).values(&data).execute(connection);
+    //     let _result = diesel::insert_into(users::table).values(&data).execute(connection);
 
-        users::table
-        .order(users::id.desc())
-        .first(connection)
-        .unwrap()
-    }
+    //     users::table
+    //     .order(users::id.desc())
+    //     .first(connection)
+    //     .unwrap()
+    // }
 
-    pub async fn delete(connection: &mut Connection, user_id: Uuid) -> bool {
-        diesel::delete(users::table.find(user_id)).execute(connection).is_ok()
-    }
+    // pub async fn delete(connection: &mut Connection, user_id: Uuid) -> bool {
+    //     diesel::delete(users::table.find(user_id)).execute(connection).is_ok()
+    // }
 
-    pub async fn delete_all(connection: &mut Connection) -> bool {
-        diesel::delete(all_users).execute(connection).is_ok()
-    }
+    // pub async fn delete_all(connection: &mut Connection) -> bool {
+    //     diesel::delete(all_users).execute(connection).is_ok()
+    // }
 }
