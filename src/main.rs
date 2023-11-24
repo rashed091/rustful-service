f2dc226bd8f237f872545626b5576a96826ad56e
mrasheduzzaman <rashed091nsu@gmail.com>
2023-11-24 09:07:11 +0600

    Implement user api controller and services

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,10 @@ use salvo::prelude::*;
 use std::env;
 
 #[handler]
-async fn hello() -> &'static str {
-    "Hello World"
+async fn health() -> &'static str {
+    "I'm alive"
 }
+
 
 #[tokio::main]
 async fn main() {
@@ -25,7 +26,19 @@ async fn main() {
     let port = env::var("PORT").expect("PORT is not set in .env file");
     let server_url = format!("{host}:{port}");
 
-    let router = Router::new().get(user_controller::all_users);
+    let router = Router::new()
+        .push(Router::with_path("/healthz").get(health))
+        .push(Router::with_path("/users")
+                .get(user_controller::get_all_users)
+                .post(user_controller::create_user)
+                .delete(user_controller::delete_all_users)
+                .push(Router::with_path("<id>")
+                        .get(user_controller::get_user)
+                        .patch(user_controller::update_user)
+                        .delete(user_controller::delete_user)
+                    )
+            );
+
     let acceptor = TcpListener::new(server_url).bind().await;
 
     Server::new(acceptor).serve(router).await;
